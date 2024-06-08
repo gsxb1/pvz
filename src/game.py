451ef5906cd99eaa2cf93ce13d2dab07ffcf2efb
gsxb1 <2024596740@qq.com>
2024-06-08 10:31:21 +0800

    加向日葵， 鼠标点击事件

--- src/game.py
+++ src/game.py
@@ -1,4 +1,5 @@
 # 游戏的主逻辑
+import pygame
 import image
 import sunflower
 from const import *
@@ -9,11 +10,9 @@ class Game(object):
         self.back = image.Image("../pic/other/back.png", 0 ,(0,0), GAME_SIZE, 0)
         self.plants = []           #向日葵列表
         self.summons = []          #召唤物列表
-        for i in range(GAME_COUNT[0]):
+        for i in range(3):
             for j in range(GAME_COUNT[1]):
-                pos = LEFT_TOP[0] + i * GRID_SIZE[0], LEFT_TOP[1] + j * GRID_SIZE[1]
-                sf = sunflower.SunFlower(3, pos)
-                self.plants.append(sf)
+                self.addSunFlower(i, j)
 
 
     def draw(self):
@@ -32,5 +31,25 @@ class Game(object):
                 self.summons.append(summon)         #由我的表格来管理
         for summon in self.summons:
             summon.update()
+
+    def addSunFlower(self, x ,y):
+        pos = LEFT_TOP[0] + x * GRID_SIZE[0], LEFT_TOP[1] + y * GRID_SIZE[1]
+        sf = sunflower.SunFlower(3, pos)
+        self.plants.append(sf)
+
+    def cheackLooct(self, mosousePos):
+        # 捡阳光
+        pass
+
+    def checkAddPlant(self, mosousePos, objId):
+        # 添加植物
+        pass
+
+    def mouseClickHandler(self, btn):
+        mosousePos = pygame.mouse.get_pos()
+        self.cheackLooct(mosousePos)
+        self.checkAddPlant(mosousePos)
+
+
 
 
--- src/main.py
+++ src/main.py
@@ -13,14 +13,17 @@ DS = pygame.display.set_mode(GAME_SIZE)
 # 对Game进行实例化
 game = Game(DS)
 
-# 检查操作系统的事件
-# 如果事件类型是否为QUIT（表示用户点击了窗口的关闭按钮）（pygame.quit()）
-# 就关闭pygame，并关闭python解释器（sys.exit()）
+# 无限循环检查操作系统的事件
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
+            # 如果事件类型是否为QUIT（表示用户点击了窗口的关闭按钮）（pygame.quit()）
             pygame.quit()
             sys.exit()
+            # 关闭pygame，并关闭python解释器（sys.exit()）
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            # 如果事件类型为鼠标点击，获取鼠标点击的是左键还是右键
+            game.mouseClickHandler(event.button)
     # 创建一块白色的画布
     DS.fill((255, 255, 255))
 
