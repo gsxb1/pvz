2143fbc63aee51eef35a37feca3d1dd216a78de7
gsxb1 <2024596740@qq.com>
2024-06-08 12:04:31 +0800

    重复种，可以种在外面 修复以上提到的bug

--- src/game.py
+++ src/game.py
@@ -4,16 +4,19 @@ import image
 import sunflower
 from const import *
 
+
 class Game(object):
     def __init__(self, ds):
         self.ds = ds
-        self.back = image.Image("../pic/other/back.png", 0 ,(0,0), GAME_SIZE, 0)
-        self.plants = []           #向日葵列表
-        self.summons = []          #召唤物列表
-        for i in range(3):
+        self.back = image.Image("../pic/other/back.png", 0, (0, 0), GAME_SIZE, 0)
+        self.plants = []  # 向日葵列表
+        self.summons = []  # 召唤物列表
+        self.hasPlant = []
+        for i in range(GAME_COUNT[0]):
+            col = []
             for j in range(GAME_COUNT[1]):
-                self.addSunFlower(i, j)
-
+                col.append(0)
+            self.hasPlant.append(col)
 
     def draw(self):
         self.back.draw(self.ds)
@@ -26,9 +29,9 @@ class Game(object):
         self.back.update()
         for plant in self.plants:
             plant.update()
-            if plant.hasSummon():                   #如果植物身上有召唤物
-                summon = plant.doSummon()           #我来创建一个对象
-                self.summons.append(summon)         #由我的表格来管理
+            if plant.hasSummon():  # 如果植物身上有召唤物
+                summon = plant.doSummon()  # 我来创建一个对象
+                self.summons.append(summon)  # 由我的表格来管理
         for summon in self.summons:
             summon.update()
 
@@ -37,7 +40,10 @@ class Game(object):
         y = (pos[1] - LEFT_TOP[1]) // GRID_SIZE[1]
         return x, y
 
-    def addSunFlower(self, x ,y):
+    def addSunFlower(self, x, y):
+        if self.hasPlant[x][y] == 1:
+            return
+        self.hasPlant[x][y] = 1
         pos = LEFT_TOP[0] + x * GRID_SIZE[0], LEFT_TOP[1] + y * GRID_SIZE[1]
         sf = sunflower.SunFlower(SUNFLOWER_ID, pos)
         self.plants.append(sf)
@@ -49,6 +55,10 @@ class Game(object):
     def checkAddPlant(self, mosousePos, objId):
         # 添加植物
         x, y = self.getIndexByPos(mosousePos)
+        if x < 0 or x >= GAME_COUNT[0]:
+            return
+        if y < 0 or y >= GAME_COUNT[1]:
+            return
         if objId == SUNFLOWER_ID:
             self.addSunFlower(x, y)
 
@@ -57,7 +67,4 @@ class Game(object):
         self.cheackLooct(mosousePos)
         if btn == 1:
             self.checkAddPlant(mosousePos, SUNFLOWER_ID)
-
-
-
 
